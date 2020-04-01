93f345197f1d43b8f9196b5f268829a3a8a19ca2
jonnysfarmer <john.farmer@latimerross.com>
2020-04-01 15:50:01 +0100

    basic frontend set up

---
+++ career-path/frontend/src/app.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import { Switch, Route, HashRouter } from 'react-router-dom'
+
+// import ResponsiveDrawer from './components/Navbar'
+// import Home from './components/Home'
+// import Register from './components/pages/Register'
+// import Login from './components/pages/Login'
+// import Prescriptions from './components/pages/Prescriptions'
+// import Prescription from './components/pages/Prescription'
+// import Profile from './components/pages/Profile'
+// import Logout from './components/Logout'
+// import EditProfile from './components/pages/EditProfile'
+// import CreatePrescription from './components/pages/CreatePrescription2'
+// import EditPrescription from './components/pages/EditPrescription'
+// import EditReminder from './components/pages/EditReminder'
+
+
+// //STYLES FOR OVERWRITING MATERIAL UI
+// import './styles/main.css'
+
+const App = () => {
+
+
+  return (
+    <div>
+      Hello World
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
--- career-path/project/urls.py
+++ career-path/project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # path('', include('frontend.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('jwt_auth.urls')),
     path('api/', include('Jobs.urls')),
